2f19cc6b23dcf8d9ea2c94e9ca6f32f59511f928
LocND <loc.nd247@gmail.com>
2018-05-21 09:07:27 +0700

    change mode files

--- config/db.php
+++ config/db.php
@@ -2,17 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=truyentranh',
+    'dsn' => 'mysql:host=localhost;dbname=muontruyen',
+//    'dsn' => 'mysql:host=localhost;dbname=truyentranh',
     'username' => 'root',
-    'password' => '123456',
+    'password' => 'l!o@c#n$g%u^y&e*n',
+//    'password' => '123456',
     'charset' => 'utf8',
-
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=muontruyen',
-//    'username' => 'root',
-//    'password' => 'l!o@c#n$g%u^y&e*n',
-//    'charset' => 'utf8',
-
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
