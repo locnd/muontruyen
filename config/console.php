5b835e65dcdd795ce19b94559f1ef66ef2b3abd0
LocND <loc.nd247@gmail.com>
2018-05-21 10:31:45 +0700

    add function to cron

--- config/console.php
+++ config/console.php
@@ -2,7 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-
+$functions = require __DIR__ . '/functions.php';
 $config = [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
