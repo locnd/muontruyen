ce25df23d181bfad15c7ae7ea08f0a053e3626f4
LocND <loc.nd247@gmail.com>
2018-05-30 17:19:48 +0700

    fix book list

--- views/admin/book/index.php
+++ views/admin/book/index.php
@@ -24,10 +24,9 @@
                                         <th><?php echo 'Image';?></th>
                                         <th><?php echo 'Url';?></th>
                                         <th><?php echo 'Chapters';?></th>
-                                        <th><?php echo 'Views';?></th>
                                         <th><?php echo 'Status';?></th>
+                                        <th><?php echo 'Release date';?></th>
                                         <th><?php echo 'Created date';?></th>
-                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -38,17 +37,9 @@
                                             <td><img class="show_profile_image mini-image" src="<?php echo $book->get_image(); ?>"></td>
                                             <td><a href="<?php echo $book->url; ?>" target="_blank"><?php echo $book->url; ?></a></td>
                                             <td><?php echo count($book->chapters); ?></td>
-                                            <td><?php echo $book->count_views; ?></td>
                                             <td><?php echo $book->status == 1 ? 'Active' : 'Inactive'; ?></td>
                                             <td><?php echo date('d-m-Y H:i:s',strtotime($book->created_at)); ?></td>
-                                            <td>
-                                                <a href="/admin/book/detail/<?php echo $book->id; ?>" class="btn btn-success btn-icon btn-circle btn-lg m-r-5"><i class="fa fa-search"></i></a>
-                                                <?php if(empty($book->deleted_at)) { ?>
-                                                <a onclick="delete_item('book', <?php echo $book->id; ?>)" class="btn btn-danger btn-icon btn-circle btn-lg"><i class="fa fa-trash"></i></a>
-                                                <?php } else { ?>
-                                                <a onclick="restore_item('book', <?php echo $book->id; ?>)" class="btn btn-danger btn-icon btn-circle btn-lg"><i class="fa fa-refresh"></i></a>
-                                                <?php } ?>
-                                            </td>
+                                            <td><?php echo date('d-m-Y H:i:s',strtotime($book->release_date)); ?></td>
                                         </tr>
                                     <?php } ?>
                                 </tbody>
