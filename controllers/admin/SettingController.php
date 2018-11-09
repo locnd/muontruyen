5c04bc57b9e2dbd9fff96a98b406bf9a89ec1f08
LocND <loc.nd247@gmail.com>
2018-11-09 14:07:53 +0700

    delete chapter invalid images

--- controllers/admin/SettingController.php
+++ controllers/admin/SettingController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers\admin;
 
+use app\models\Chapter;
+use app\models\Image;
 use yii\web\Controller;
 use app\models\Setting;
 use Yii;
@@ -22,7 +24,7 @@ class SettingController extends Controller
         }
         return parent::beforeAction($action);
     }
-    
+
     public function actionIndex() {
         $this->view->params['page_id'] = 'setting';
 
@@ -30,6 +32,29 @@ class SettingController extends Controller
         return $this->render('/admin/setting/index', array(
             'settings' => $settings
         ));
+    }
+    public function actionDeleteImagesBase64() {
+        $images = Image::find()->where(['like', 'image_source', 'data:image'])->groupBy(['chapter_id'])->all();
+        $total = count($images);
+        foreach ($images as $image) {
+            $chapter = Chapter::find()->where(array('id'=>$image->chapter_id))->one();
+            if(empty($chapter) || $chapter->will_reload == 1) {
+                continue;
+            }
+            $chapter->status = Chapter::INACTIVE;
+            $chapter->will_reload = 1;
+            $chapter->save();
+            Yii::$app->db->createCommand()
+                ->delete('dl_images', ['chapter_id' => $chapter->id])
+                ->execute();
+            Yii::$app->db->createCommand()
+                ->delete('dl_bookmarks', ['chapter_id' => $chapter->id])
+                ->execute();
+            clear_book_cache($chapter->book);
+            Yii::$app->cache->delete('chapter_detail_'.$chapter->id);
+        }
+        echo 'Make reload '.$total.' chapters';
+        exit();
     }
 }
 
