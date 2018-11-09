700a1bdbc59c4392eb9728cb5c7f5ed1ff13e2a6
LocND <loc.nd247@gmail.com>
2018-11-09 21:32:42 +0700

    backup reload

--- nodejs/reload_chapter.js
+++ nodejs/reload_chapter.js
@@ -201,24 +201,6 @@ function clone_chap(chap) {
     });
 }
 function parse_chapter(chap, body) {
-    var nodes = body.split('page-chapter');
-    if (nodes.length < 2) {
-        clone_chap(chap);
-    } else {
-        chap.total_image = nodes.length - 2;
-        chap.count_image = 0;
-        for (var i = 1; i < nodes.length-1; i++) {
-            var image_str = nodes[i].trim();
-            var image = get_image_url(image_str);
-            if (image == '') {
-                chap.count_image++;
-            } else {
-                console.log('create image ' + i);
-                create_image(chap, image, i);
-            }
-        }
-    }
-    /*
     var dom = parser.parseFromString(body, 'text/html');
     if(dom == null || typeof(dom) == 'undefined') {
         clone_chap(chap);
@@ -226,7 +208,6 @@ function parse_chapter(chap, body) {
         if(is_error_page(dom)) {
             count_chapter++;
         } else {
-
             var nodes = dom.getElementsByClassName('page-chapter');
             if (nodes.length == 0) {
                 clone_chap(chap);
@@ -247,7 +228,6 @@ function parse_chapter(chap, body) {
             }
         }
     }
-    */
 }
 function get_request() {
     if(use_proxy) {
